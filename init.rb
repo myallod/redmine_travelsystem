eadafa9de7136378381b91c9c8131827c82981f4
cron travelsystem.ru <root@gigant.travelsystem.ru>
2013-09-12 01:14:40 +0400

    First commit

---
+++ init.rb
@@ -0,0 +1,9 @@
+Redmine::Plugin.register :redmine_travelsystem do
+  name 'Redmine Travelsystem plugin'
+  author 'lek'
+  description 'Patches for Redmine'
+  version '0.0.1'
+  url 'http://travelsystem.ru'
+  author_url 'http://travelsystem.ru'
+  requires_redmine :version_or_higher => '2.2.0'
+end
